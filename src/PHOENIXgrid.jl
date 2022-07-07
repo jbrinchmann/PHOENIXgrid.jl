f4ab363317f3737d1c10403f30d6772080ad8986
Jarle Brinchmann <jbrinchmann@gmail.com>
2022-07-07 21:45:01 +0100

    Added packages and including further files

--- src/PHOENIXgrid.jl
+++ src/PHOENIXgrid.jl
@@ -3,8 +3,13 @@ module PHOENIXgrid
 using Printf
 using CSV # DataFrames
 using FITSIO
+using Plots
+import Statistics: median
+import Random: randn
+import Interpolations: LinearInterpolation
 
 include("io.jl")
-
+include("fit.jl")
+include("mock.jl")
 
 end # module
