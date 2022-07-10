250b3dd3c525d5b19ddac03a7a606538ab0b6471
Jarle Brinchmann <jbrinchmann@gmail.com>
2022-07-10 13:02:42 +0100

    Added some exported functions and imported visualise.jl

--- src/PHOENIXgrid.jl
+++ src/PHOENIXgrid.jl
@@ -8,8 +8,16 @@ import Statistics: median
 import Random: randn
 import Interpolations: LinearInterpolation
 
+export load_grid_summary, load_one_spectrum, load_grid_of_spectra,
+    make_spectrum_mask, mask_spectrum,
+    fit_one_spectrum, fit_grid, fill_p, create_prior_grid, marginalise,
+    summarise_results, print_results, get_best_fit
+
+
+
 include("io.jl")
 include("fit.jl")
 include("mock.jl")
+include("visualise.jl")
 
 end # module
